c8687c28e436faf5fe5f2443d3a0d10975b2940b
kramstyles <mark.eke@decagon.dev>
2022-05-29 00:51:17 +0100

    changed non field errors

--- project/settings.py
+++ project/settings.py
@@ -132,3 +132,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authentication.User'
+
+REST_FRAMEWORK = {
+    'NON_FIELD_ERRORS_KEY': 'errors', # Changed non field to errors
+}
