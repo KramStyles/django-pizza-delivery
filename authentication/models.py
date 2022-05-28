b4b8c3876bf2a58413faaa58138fb3ec1b8d4bde
kramstyles <mark.eke@decagon.dev>
2022-05-28 18:37:55 +0100

    added code for creating user

--- authentication/models.py
+++ authentication/models.py
@@ -1,10 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
 
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
-        pass
+        if not email:
+            raise ValueError(_("Email is required!"))
+
+        email = self.normalize_email(email) # Changes the case of the domain part of the email
+        new_user = self.model(email=email, **extra_fields)
+
+        new_user.set_password(password)
+        new_user.save()
+
+        return new_user
 
     def create_superuser(self, email, password, **extra_fields):
         pass
