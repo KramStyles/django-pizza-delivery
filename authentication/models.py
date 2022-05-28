e8594d694586f4e9b8bd2ce3885528d84a7e7978
kramstyles <mark.eke@decagon.dev>
2022-05-28 18:27:51 +0100

    created skeleton method to overwrite user creation

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        pass
+
+    def create_superuser(self, email, password, **extra_fields):
+        pass
