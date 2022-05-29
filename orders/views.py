591daefb142944d571c470f8e2704a58b8c8862c
kramstyles <mark.eke@decagon.dev>
2022-05-29 23:09:28 +0100

    finished with put request

--- orders/views.py
+++ orders/views.py
@@ -41,8 +41,14 @@ class OrderDetailView(generics.GenericAPIView):
 
         return response.Response(data=serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request, order_id):
-        pass
+    def put(self, request, order_id):
+        serializer = self.serializer_class(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save(customer=request.user)
+            
+            return response.Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
+        return response.Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, order_id):
         pass
