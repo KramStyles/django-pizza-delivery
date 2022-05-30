aea70acd965956dfec641cc804a034e43bc8ffdb
kramstyles <mark.eke@decagon.dev>
2022-05-30 18:04:36 +0100

    reinplemented django on heroku

--- project/settings.py
+++ project/settings.py
@@ -16,6 +16,7 @@ from datetime import timedelta
 from decouple import config
 
 import dj_database_url
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -165,3 +166,5 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+django_on_heroku.settings(locals())
