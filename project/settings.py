70ae7baba804196490ee93c3318fb3f4b1fd41f4
kramstyles <mark.eke@decagon.dev>
2022-05-30 18:06:47 +0100

    attempting default for postgres db

--- project/settings.py
+++ project/settings.py
@@ -95,7 +95,7 @@ if DEBUG:
         }
 else:
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-    DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+    DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL', default='hello'))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
